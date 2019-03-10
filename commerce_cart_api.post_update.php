912d5b5da03bfdedff89e0386d0ab525842f5bfc
mglaman <mglaman@2416470.no-reply.drupal.org>
2019-03-10 10:08:26 -0500

    Issue #2996228 by mglaman: Add update hook to install commerce_cart_clear configuration

---
+++ commerce_cart_api.post_update.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Post update functions for Commerce Cart API.
+ */
+
+/**
+ * Ensures the cart clear resource configuration is present.
+ */
+function commerce_cart_api_post_update_install_cart_clear_resource() {
+  $config_updater = \Drupal::getContainer()->get('commerce.config_updater');
+  $config_updater->import([
+    'rest.resource.commerce_cart_clear'
+  ]);
+}
